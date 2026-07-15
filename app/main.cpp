2272dbc61d8a6d35e6fcfa8105847af2fd7433da
kovelj <veljkoilic86@gmail.com>
2026-07-15 11:56:31 +0200

    Adding TEST_DIR unified for all OS's and creating CMake insine src folder.

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,22 @@
+#include "filesystem"
 #include "Simulator.h"
 
 void run_tests() {
 	std::vector<int> test_vector = {0,1,2,3,4,5};
-	
+	std::filesystem::path test_dir(TEST_DIR);
+
 	Simulator simulator;
-	for (const auto test_number : test_vector) {
-		simulator.loadCircuit("../test/test_" + std::to_string(test_number) + ".txt");
-		simulator.simulate("../test/test_" + std::to_string(test_number) + "_output.txt");
+	for (auto test_number : test_vector) {
+		auto input_path = test_dir / ("test_" + std::to_string(test_number) + ".txt");
+		auto output_path = test_dir / ("test_" + std::to_string(test_number) + "_output.txt");
+
+		simulator.loadCircuit(input_path);
+		simulator.simulate(output_path);
 	}
 }
 
-int main() 
-{
+int main() {
+
 	run_tests();
 	return 0;
 }
